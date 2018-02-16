db03ad4e078debe6e4b8418cfb1838413800f432
David García <ccristhiangarcia@gmail.com>
2018-02-16 10:55:55 -0500

    Create admin user

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UserTableSeeder extends Seeder
+{
+    public function run()
+    {
+        $this->createAdmin();
+    }
+
+    public function createAdmin()
+    {
+        return User::create([
+            'name' => 'David García', 
+            'email' => 'ccristhiangarcia@gmail.com', 
+            'password' => bcrypt('secret'), 
+            'remember_token' => str_random(25)
+        ]);
+    }
+}
