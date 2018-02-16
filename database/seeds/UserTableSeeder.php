c93c7413ae6b322a1cc4744ad226c5260207c466
David García <ccristhiangarcia@gmail.com>
2018-02-16 11:21:57 -0500

    Using Faker to load dozens of records

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -2,15 +2,17 @@
 
 use Illuminate\Database\Seeder;
 use TeachMe\Entities\User;
+use Faker\Factory as Faker;
 
 class UserTableSeeder extends Seeder
 {
     public function run()
     {
         $this->createAdmin();
+        $this->createUsers();
     }
 
-    public function createAdmin()
+    protected function createAdmin()
     {
         return User::create([
             'name' => 'David García', 
@@ -18,5 +20,19 @@ class UserTableSeeder extends Seeder
             'password' => bcrypt('secret'), 
             'remember_token' => str_random(25)
         ]);
+    }
+
+    protected function createUsers()
+    {
+        $faker = Faker::create();
+
+        for ($i = 1; $i < 50; $i++) { 
+            User::create([
+                'name' => $faker->name, 
+                'email' => $faker->email,
+                'password' => bcrypt('secret'), 
+                'remember_token' => str_random(25)
+            ]);
+        }
     }
 }
