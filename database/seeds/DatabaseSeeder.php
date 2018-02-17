37b281d08636acc1a78e25d1d0156c104e337d64
David García <ccristhiangarcia@gmail.com>
2018-02-17 13:00:01 -0500

    Truncate tables with foreign keys restrictions

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -5,6 +5,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class DatabaseSeeder extends Seeder {
 
+	protected $tables = [
+		'users', 'tickets', 'votes', 'comments', 'password_resets'
+	];
+
 	/**
 	 * Run the database seeds.
 	 *
@@ -14,7 +18,24 @@ class DatabaseSeeder extends Seeder {
 	{
 		Model::unguard();
 
+		$this->checkForeignKeys(false);
+		$this->truncate($this->tables);
+		$this->checkForeignKeys(true);
+
 		$this->call('UserTableSeeder');
+	}
+
+	protected function truncate(array $tables)
+	{
+		foreach ($tables as $table) {
+			DB::table($table)->truncate();
+		}
+	}
+
+	protected function checkForeignKeys($check)
+	{
+		$check = $check ? '1' : '0';
+		DB::statement('SET FOREIGN_KEY_CHECKS = ' . $check);
 	}
 
 }
