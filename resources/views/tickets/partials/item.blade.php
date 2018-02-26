ce07a2db90629d2ced7d2ec43f614b53a7ede779
David García <ccristhiangarcia@gmail.com>
2018-02-26 08:01:06 -0500

    Validate if authenticate to display details link

--- resources/views/tickets/partials/item.blade.php
+++ resources/views/tickets/partials/item.blade.php
@@ -17,12 +17,13 @@
                 No votar
             </button>
         {!! Form::close() !!}
-
-        <a href="{{ route('tickets.details', $ticket) }}">
-            <span class="votes-count">{{ $ticket->num_votes }} votos</span>
-            - <span class="comments-count">{{ $ticket->num_comments }} comentarios</span>.
-        </a>
-
+        
+        @if (Auth::check())
+            <a href="{{ route('tickets.details', $ticket) }}">
+                <span class="votes-count">{{ $ticket->num_votes }} votos</span>
+                - <span class="comments-count">{{ $ticket->num_comments }} comentarios</span>.
+            </a>
+        @endif
     <p class="date-t"><span class="glyphicon glyphicon-time"></span> {{ $ticket->created_at->format('d/m/Y h:ia') }}</p>
     </p>
 </div>
