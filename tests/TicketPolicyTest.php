a01131b9c16efc2d2bcf050f32b7cae1a7a4b13e
David García <ccristhiangarcia@gmail.com>
2018-03-01 13:24:08 -0500

    Create tests to prevent users select resource twice and administrators select two resources

--- tests/TicketPolicyTest.php
+++ tests/TicketPolicyTest.php
@@ -53,4 +53,57 @@ class TicketPolicyTest extends TestCase
         // Then
         $this->assertTrue(Gate::forUser($admin)->allows('selectResource', $ticket));
     }
+
+    public function test_prevent_users_for_selecting_resource_twice()
+    {
+        // Having
+        $user = seed('User');
+        $ticket = seed('Ticket', [
+            'user_id' => $user->id, 
+            'status' => 'closed'
+        ]);
+
+        // When
+        $policy = new TicketPolicy();
+
+        // Then
+        $this->assertFalse($policy->selectResource($user, $ticket));
+    }
+
+    public function test_administrators_select_two_resources()
+    {
+        // Having
+        $user = seed('User');
+        $ticket = seed('Ticket', ['user_id' => $user->id]);
+
+        $comment1 = seed('Comment', [
+            'ticket_id' => $ticket->id, 
+            'link' => 'https://styde.net'
+        ]);
+
+        $comment2 = seed('Comment', [
+            'ticket_id' => $ticket->id, 
+            'link' => 'https://platzi.com'
+        ]);
+
+        // When
+        $ticket->assignResource($comment1);
+        $ticket->assignResource($comment2);
+
+        // Then
+        $this->seeInDatabase('tickets', [
+            'id' => $ticket->id, 
+            'link' => 'https://platzi.com', 
+        ]);
+        
+        $this->seeInDatabase('comments', [
+            'id' => $comment1->id,
+            'selected' => false, 
+        ]);
+
+        $this->seeInDatabase('comments', [
+            'id' => $comment2->id,
+            'selected' => true, 
+        ]);
+    }
 }
